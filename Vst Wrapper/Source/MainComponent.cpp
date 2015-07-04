a10b34525fa9b0d0beceb5646c89ae335e1aa983
Bk8 <bk8biokiller8@gmail.com>
2015-07-04 18:15:12 -0500

    Vst Wrapper
    A simple vst wrapper using Juce, some plugins may not work properly
    because exception handling is not implemented yet

---
+++ Vst Wrapper/Source/MainComponent.cpp
@@ -0,0 +1,37 @@
+/*
+  ==============================================================================
+
+    This file was auto-generated!
+
+  ==============================================================================
+*/
+
+#include "MainComponent.h"
+
+
+//==============================================================================
+MainContentComponent::MainContentComponent()
+{
+	// Add your application's initialisation code here..
+
+	
+			
+			
+
+}
+
+MainContentComponent::~MainContentComponent()
+{
+}
+
+void MainContentComponent::paint (Graphics& g)
+{
+
+}
+
+void MainContentComponent::resized()
+{
+    // This is called when the MainContentComponent is resized.
+    // If you add any child components, this is where you should
+    // update their positions.
+}
